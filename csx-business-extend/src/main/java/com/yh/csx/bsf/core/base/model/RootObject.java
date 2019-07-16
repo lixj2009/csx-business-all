f8dd6def5057a07a2c6677aa7de10c1f9a5d1feb
Lixuejie <lixuejie@yonghu.cn>
2019-07-16 14:35:44 +0800

    覆盖bsf core包类

---
+++ csx-business-extend/src/main/java/com/yh/csx/bsf/core/base/model/RootObject.java
@@ -0,0 +1,9 @@
+package com.yh.csx.bsf.core.base.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by yanglikai on 2019/5/28.
+ */
+public abstract class RootObject implements Serializable {
+}
